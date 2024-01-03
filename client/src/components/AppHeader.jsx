49330432165cce6d987ed0d914e329a5cc20fa84
Astrick <mypiarenoo@gmail.com>
2024-01-03 14:40:34 +0700

    (Fixed): Sync username to AppHeader and sync products to ViewProductPage

--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { UserContext } from "../context/context";
+
 function AppHeader() {
+  const userData = useContext(UserContext);
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {userData.username}</h2>
     </div>
   );
 }
--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,18 +1,42 @@
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../context/context";
+import { useParams } from "react-router-dom";
+import products from "../data/products";
 
 function ViewProductPage() {
+  const params = useParams();
   const navigate = useNavigate();
+  const userData = useContext(UserContext);
+
+  const product = products.filter((p) => p.id == params.id)[0];
+
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
   return (
     <div>
       <h1>View Product Page</h1>
       <div className="view-product-container">
         <h2>Product Title</h2>
         <p>Content</p>
+        <div className="product">
+          <div className="product-preview">
+            <img src={product.image} alt={product.name} width="250" height="250" />
+          </div>
+          <div className="product-detail">
+            <h1>Product name: {product.name}</h1>
+            <h2>Product price: {product.price}</h2>
+            <p>Product description: {product.description}</p>
+            <div className="product-actions"></div>
+          </div>
+        </div>
       </div>
 
       <div className="product-promotion-box">
-        <h2>คุณคือสมาชิกในระดับ (x) ดังนั้นคุณได้สิทธิพิเศษลด 50%</h2>
-        <button onClick={() => alert("🥳🥳🥳🥳")}>กดรับสิทธิ</button>
+        <h2>คุณคือสมาชิกในระดับ {userData.level} ดังนั้นคุณได้สิทธิพิเศษลด 50%</h2>
+        <button onClick={() => alert("ล้อลเล่น สมน้ำหน้า!!🥳🥳🥳🥳")}>กดรับสิทธิ</button>
       </div>
 
       <button onClick={() => navigate("/")}>Back to Home</button>

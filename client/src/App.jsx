bcde1ff23b52cb9ee48741b897aebbf65e576682
bread <chalida.bread@gmail.com>
2024-01-04 08:57:54 +0700

    fix:rename components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
-import User from "./components/UserContext.jsx";
+import UserContext from "./components/UserContext.jsx";
 
 function App() {
   const userData = {
@@ -14,14 +14,14 @@ function App() {
 
   return (
     <div className="App">
-      <User.Provider value={userData}>
+      <UserContext.Provider value={userData}>
         <Router>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/product/view/:id" element={<ViewProductPage />} />
           </Routes>
         </Router>
-      </User.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
